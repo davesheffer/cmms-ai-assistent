b7710bf3dd3b83fd9926a13ba45c5b7d018fa871
davesheffer <dave.sheffer1@gmail.com>
2024-11-11 12:00:17 +0200

    done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { Header } from "@/components/Header";
 import { AIAssistantChat } from "@/components/AI-assistent";
+import { Toaster } from "sonner";
 
 const geistSans = localFont({
 	src: "./fonts/GeistVF.woff",
@@ -28,6 +29,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+				<Toaster position="bottom-left" />
 				<Header />
 				{children}
 				<AIAssistantChat />
