323115e15c76346fe74b9d63583721d6992c8da6
davesheffer <dave.sheffer1@gmail.com>
2024-11-11 12:58:00 +0200

    done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { Header } from "@/components/Header";
+import Header from "@/components/Header";
 import { AIAssistantChat } from "@/components/AI-assistent";
 import { Toaster } from "sonner";
 
